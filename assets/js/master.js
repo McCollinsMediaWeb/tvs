237cc60c3e0354d43b9f46ec472e2f1363a3a18c
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-09-10 00:55:04 +0530

    ccc

--- assets/js/master.js
+++ assets/js/master.js
@@ -33,4 +33,28 @@ $(document).ready(
     }
   );
 
+  $(document).ready(function(){
+  
+            $(".MenuButton , .CloseButton1 ,.MenuActive1 ").click(function () {
+                jQuery("body").toggleClass("menuPopupActivated");
+                windowHeight = jQuery(window).innerHeight();
+                PopupHeaderHeight1= jQuery(".PopUpHeaderBoxWrap1").innerHeight();
+                PopuuFooterHeight1= jQuery(".PopUpFooterBoxWrap1").innerHeight();
+                totalGap = PopupHeaderHeight1 + PopuuFooterHeight1;
+                jQuery('.PopupContentBox1').css('height', (windowHeight - totalGap )+'px');
+                
+            });
+            $(".contactPopupButton , .contactPopupClose , .Contactusnow").click(function () {
+                jQuery("body").toggleClass("contactPopupActivated");
+                
+                windowHeight = jQuery(window).innerHeight();
+                PopupHeaderHeight2= jQuery(".PopUpHeaderBoxWrap2").innerHeight();
+                PopuuFooterHeight2= jQuery(".PopUpFooterBoxWrap2").innerHeight();
+                totalGap = PopupHeaderHeight2 + PopuuFooterHeight2;
+                jQuery('.PopupContentBox2').css('height', (windowHeight - totalGap )+'px');
+                
+            });
+            
+  });
+
   
