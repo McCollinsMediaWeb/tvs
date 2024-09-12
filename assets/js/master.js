11d593be954252e64a551a20810e84f962a16384
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-09-12 14:23:57 +0530

    1515

--- assets/js/master.js
+++ assets/js/master.js
@@ -76,3 +76,77 @@ $(document).ready(
         $('.TabItemContent[data-tab="' + tabId + '"]').show();
     });
 });
+
+
+$(".team").slick({
+  dots: true,
+  infinite: true,
+  speed: 1500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  loop: true,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 2,
+        centerMode: true,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: true,
+      },
+    },
+  ],
+});
+$(".TabItemsMobie").slick({
+  dots: true,
+  infinite: true,
+  speed: 1500,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  loop: true,
+  arrows: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        centerMode: true,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerMode: true,
+      },
+    },
+  ],
+});
