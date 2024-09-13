9a9baf038707896e5fc34841a6b9ecfbc8f4ac0b
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2024-09-13 12:15:16 +0530

    sss

--- assets/js/master.js
+++ assets/js/master.js
@@ -150,3 +150,40 @@ $(".TabItemsMobie").slick({
     },
   ],
 });
+$(".GalleryItemSlider").slick({
+  dots: true,
+  infinite: false,
+  speed: 1500,
+  slidesToShow: 5,
+  slidesToScroll: 3,
+  loop: false,
+  arrows: true,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: false,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        centerMode: true,
+      },
+    },
+    {
+      breakpoint: 570,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerMode: true,
+      },
+    },
+  ],
+});
